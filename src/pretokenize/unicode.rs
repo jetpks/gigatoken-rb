5ba4b1904633b0eadb32b6170925789c2eeeee78
Marcel Rød <marcelroed@gmail.com>
2025-10-15 19:22:21 -0700

    Combine ascii check with unicode check in new unicode functions

--- src/pretokenize/unicode.rs
+++ src/pretokenize/unicode.rs
@@ -34,3 +34,36 @@ pub(crate) fn is_number(c: char) -> bool {
 pub(crate) fn is_separator(c: char) -> bool {
     is_gc_separator(get_general_category(c))
 }
+
+#[inline]
+pub(crate) fn is_letter_complete(c: char) -> bool {
+    if c.is_ascii() {
+        return c.is_ascii_alphabetic();
+    }
+    is_letter(c)
+}
+
+#[inline]
+pub(crate) fn is_number_complete(c: char) -> bool {
+    if c.is_ascii() {
+        return c.is_ascii_digit();
+    }
+    is_number(c)
+}
+
+#[inline]
+pub(crate) fn is_separator_complete(c: char) -> bool {
+    if c.is_ascii() {
+        return c.is_ascii_whitespace();
+    }
+    is_separator(c)
+}
+
+#[inline]
+pub(crate) fn is_other_complete(c: char) -> bool {
+    if c.is_ascii() {
+        return !c.is_ascii_alphanumeric() && !c.is_ascii_whitespace();
+    }
+    let gc = get_general_category(c);
+    !is_gc_letter(gc) && !is_gc_number(gc) && !is_gc_separator(gc)
+}
