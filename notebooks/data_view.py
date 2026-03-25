38c50222d183700c848b413845c1ad7f3f72e20a
Marcel Rød <marcelroed@gmail.com>
2026-03-25 14:14:19 -0700

    Data exploration to understand unicode frequencies

--- notebooks/data_view.py
+++ notebooks/data_view.py
@@ -6,21 +6,26 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
+    from collections import Counter
+    from pathlib import Path
+
     import marimo as mo
+    from emoji import demojize
+
     # Used to read files, don't redefine often
     from xopen import xopen
-    from emoji import demojize
-    from pathlib import Path
-    from collections import Counter
+
     return Counter, Path, demojize, xopen
 
 
 @app.function
-def cp_length_blocks(data, block_size: int = 256, threshold_length: int = 1, show_errors: bool = False):
+def cp_length_blocks(
+    data, block_size: int = 256, threshold_length: int = 1, show_errors: bool = False
+):
     successes = 0
     total = 0
     for window_start in range(0, len(data), block_size):
-        window = data[window_start:window_start+block_size]
+        window = data[window_start : window_start + block_size]
         for c in window:
             if c < 128:
                 clen = 1
@@ -33,7 +38,7 @@ def cp_length_blocks(data, block_size: int = 256, threshold_length: int = 1, sho
             if clen > threshold_length:
                 if show_errors:
                     try:
-                        print('[', window.decode(), ']', sep='')
+                        print("[", window.decode(), "]", sep="")
                     except:
                         pass
                 break
@@ -45,16 +50,18 @@ def cp_length_blocks(data, block_size: int = 256, threshold_length: int = 1, sho
 
 @app.cell
 def _():
-    def proportion_matches(data, fail_condition: callable, block_size: int = 64, show_errors: bool = False):
+    def proportion_matches(
+        data, fail_condition: callable, block_size: int = 64, show_errors: bool = False
+    ):
         successes = 0
         total = 0
         for window_start in range(0, len(data), block_size):
-            window = data[window_start:window_start + block_size]
+            window = data[window_start : window_start + block_size]
             for c in window:
                 if fail_condition(c):
                     if show_errors:
                         try:
-                            print('[', window.decode(), ']', sep='')
+                            print("[", window.decode(), "]", sep="")
                         except:
                             pass
                     break
@@ -69,9 +76,9 @@ def _():
 
 @app.cell
 def _(Path, demojize):
-    owt = Path("/Users/marcel/data/owt_valid.txt").read_text()
+    owt = Path("~/data/owt_valid.txt").read_text()
     owt = demojize(owt)
-    owt = owt.encode('utf-8')
+    owt = owt.encode("utf-8")
     return (owt,)
 
 
@@ -85,18 +92,21 @@ def _(owt):
 def _():
     # s = Path("/Users/marcel/data/owt_valid.txt").read_text()
     # for c in s:
-        # b = c.encode('utf-8')
-        # if len(b) == 2:
-            # print(c)
+    # b = c.encode('utf-8')
+    # if len(b) == 2:
+    # print(c)
     return
 
 
 @app.cell
 def _(demojize, xopen):
-    cc_text = xopen("/Users/marcel/data/CC-MAIN-20251005114239-20251005144239-00000.warc.wet.gz", "r").read()
+    cc_text = xopen(
+        "/Users/marcel/data/CC-MAIN-20251005114239-20251005144239-00000.warc.wet.gz",
+        "r",
+    ).read()
     cc_demojized = demojize(cc_text)
-    cc_demojized = cc_demojized.encode('utf-8')
-    cc = cc_text.encode('utf-8')
+    cc_demojized = cc_demojized.encode("utf-8")
+    cc = cc_text.encode("utf-8")
     return cc, cc_text
 
 
@@ -110,10 +120,16 @@ def _(cc):
 def _(cc, proportion_matches):
     def longer_than_2(byte):
         return byte >= 0b11100000
+
     def longer_than_3(byte):
         return byte >= 0b11110000
-    print('Proportion 2 or shorter characters', proportion_matches(cc, longer_than_2, 256))
-    print('Proportion 3 or shorter characters', proportion_matches(cc, longer_than_3, 256))
+
+    print(
+        "Proportion 2 or shorter characters", proportion_matches(cc, longer_than_2, 256)
+    )
+    print(
+        "Proportion 3 or shorter characters", proportion_matches(cc, longer_than_3, 256)
+    )
     return
 
 
@@ -131,6 +147,7 @@ def _(Counter, cc_text):
 @app.cell
 def _(cc_counts):
     from json import dump
+
     with open("/Users/marcel/data/cc_codepoint_counts.json", "w") as f:
         dump(cc_counts, f)
     return
